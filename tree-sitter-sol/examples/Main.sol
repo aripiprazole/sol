1329345d2627a0feda0973086fc263728e145da1
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-03-10 14:21:25 -0300

    chore(tree-sitter-sol): add more syntax ideas

--- tree-sitter-sol/examples/Main.sol
+++ tree-sitter-sol/examples/Main.sol
@@ -1,17 +1,35 @@
-//! Represents a nullable value
-inductive Maybe a {
-    Just : a -> Maybe a,
-    Nothing : Maybe a;
+//! Represents a natural number with a value of 0 or more, it does use
+//! the Peano representation
+inductive Nat {
+  Zero : Nat,
+  Succ : Nat -> Nat;
 }
 
-`+ : Int -> Int -> Int
-`+ = sum
+//! Represents a nullable value
+inductive Maybe a {
+  Just    : a -> Maybe a,
+  Nothing : Maybe a;
+}
+
+`+ : Nat -> Nat -> Nat
+`+ (Succ a) b = Succ (a + b)
+`+ Zero b = b
+
+`|> : a -> (a -> b) -> b
+`|> a f = f a
+
+error : String -> a = sorry
+
+Maybe.unwrap : Maybe a -> a
+Maybe.unwrap (Just a) = a
+Maybe.unwrap Nothing = error "Cannot unwrap Nothing"
 
 println : a -> IO ()
-println a = return putStrLn (show a)
+println a = println (show a) |> IO.println
 
 Main {
-    let message = "Hello world"
-    args <- getStdArgs
-    println message
+  let message = "Hello world"
+  let value = Just "Jose"
+  println (Maybe.unwrap value)
+  println message
 }
