f956a9cd792b561a036c9a249513a9da01a9669c
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-05-05 17:12:11 -0300

    style: change design to pseudolisp

--- tree-sitter-sol/examples/Main.sol
+++ tree-sitter-sol/examples/Main.sol
@@ -1,35 +1,33 @@
-(defmacro fun [args body]
-  (fold body
-    (fn [acc arg] `(-> ,arg ,acc))
-    (reverse args)))
+defmacro fun args body :=
+  fold body
+    (\acc arg. `(,arg -> ,acc))
+    (reverse args).
 
-(defmacro let [bindings expr]
+defmacro let bindings expr :=
   (if (empty? bindings)
     expr
-    `(fun ,bindings ,expr))
+    `((fun ,bindings ,expr) *bindings).
 
 ; Natural numbers
-(def Nat *
-  (-> (N : *) (N -> N) N))
+def Nat : * :=
+  (N : *) -> (N -> N) -> N.
 
-(def Succ (-> Nat Nat)
-  (fun [prev N Succ Zero] (Succ (prev N Succ Zero))))
+def Succ : (Nat -> Nat) := \prev N Succ Zero.
+  Succ (prev N Succ Zero).
 
-(def Zero Nat
-  (fun [N Succ Zero] Zero))
+def Zero : Nat := \N. Succ Zero. Zero.
 
 ; Maybe definition
-(def Maybe (-> * *)
-  (fun [T] (-> (A : *) (-> T A) A A)))
+def Maybe : (* -> *) :=
+  \t => (A : *) -> (T -> A) A -> A.
 
-(def Just (-> a (Maybe a))
-  (fun [value T Just Nothing] (Just value))
+def Just : (a -> (Maybe a)) :=
+  \value T Just Nothing => Just value.
 
-(def Nothing (Maybe a)
-  (fun [T Just Nothing] Nothing))
+def Nothing (Maybe a)
+  \T Just Nothing => Nothing.
 
-(def Maybe.unwrap (-> (Maybe a) a
-  (fun [maybe]
-    (maybe a
-      (fun [value] value) ; Just a -> a
-      sorry)))            ; Nothing -> sorry
+fun Maybe.unwrap (value : Maybe a) : a :=
+  (maybe a
+    (fun [value] value) ; Just a  => a
+    sorry)).            ; Nothing => sorry
