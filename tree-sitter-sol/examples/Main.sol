68b478cf43d3b5840cc18bfcf92f62b58e38b6c5
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-05-07 15:05:24 -0300

    feat(tree-sitter-sol): use ||

--- tree-sitter-sol/examples/Main.sol
+++ tree-sitter-sol/examples/Main.sol
@@ -1,18 +1,19 @@
 // Natural numbers
 Nat : * = (n : *) -> (n -> n) -> n
 
-Succ (prev : Nat) : Nat = \n succ# zero# =>
+Succ (prev : Nat) : Nat = |n succ# zero#|
   succ# (prev n succ# zero#) zero#
 
-Zero : Nat = \n succ# zero# => zero#
+Zero : Nat = |n succ# zero#|
+  zero#
 
 // Maybe definition
 Maybe (t : *) = (a : *) -> (t -> a) a -> a
 
-Just (value : a) : Maybe a = \t just# nothing# =>
+Just (value : a) : Maybe a = |t just# nothing#|
   just# value
 
-Nothing : Maybe a = \t just# nothing# =>
+Nothing : Maybe a = |t just# nothing#|
   nothing#
 
 Maybe.unwrap (maybe : Maybe a) : a =
