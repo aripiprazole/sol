19fff4a2c1c6a9d3a178bf873ab899fad5af05bc
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-05-07 22:03:45 -0300

    feat(sol-driver): fix church encoding tests

--- sol-driver/tests/mod.rs
+++ sol-driver/tests/mod.rs
@@ -1,12 +1,10 @@
-// use sol_diagnostic::Diagnostics;
-// use sol_driver::{make_test_suite, suite::*};
-// use sol_eval::{hir_eval, stack::Stack};
-// use sol_hir_lowering::hir_lower;
-// use sol_syntax::parse;
-// use sol_tt::Env;
-// use sol_typer::table::infer_type_table;
-// use sol_vfs::SourceFile;
-// use utils::create_package;
+use sol_diagnostic::Diagnostics;
+use sol_driver::{make_test_suite, suite::*};
+use sol_hir_lowering::hir_lower;
+use sol_syntax::parse;
+use sol_typer::table::infer_type_table;
+use sol_vfs::SourceFile;
+use utils::create_package;
 
 pub mod utils;
 
@@ -37,21 +35,3 @@ make_test_suite! {
     Ok(())
   }
 }
-
-// make_test_suite! {
-//   tests ("evaluation") {
-//     eval "eval"
-//   }
-//   run |db, source, output| {
-//     let file = SourceFile::new(&db, "repl".into(), "Repl".into(), source);
-
-//     let src = parse(&db, file);
-//     let local = create_package(&db, src, "local");
-//     let hir = hir_lower(&db, local, src);
-//     let value = hir_eval(&db, Stack::default(), Env::default(), hir);
-
-//     writeln!(output, "{:#?}", value)?;
-
-//     Ok(())
-//   }
-// }
