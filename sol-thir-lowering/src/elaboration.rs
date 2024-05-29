68aaa3c80775b9cc2a5c538d33042b21695ff903
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-05-29 20:30:20 -0300

    feat(sol-thir-lowering): implement unify catch

--- sol-thir-lowering/src/elaboration.rs
+++ sol-thir-lowering/src/elaboration.rs
@@ -1,10 +1,13 @@
+use sol_diagnostic::Diagnostics;
 use sol_thir::ElaboratedTerm;
 
 use super::*;
 
 #[salsa::tracked]
 pub fn unify_catch(db: &dyn ThirLoweringDb, ctx: Context, lhs: Value, rhs: Value) {
-    todo!()
+    if let Err(diagnostic) = lhs.unify(db, ctx, rhs) {
+        Diagnostics::push(db, diagnostic);
+    }
 }
 
 /// Insert fresh implicit applications to a term which is not
